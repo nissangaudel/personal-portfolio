71e854161031eb3157186e238e8d53b4f7683740
Nishan Gaudel <nissangaudel@gmial.com>
2022-09-25 16:57:06 -0400

    Landing Page added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { page: 'Home' });
 });
+
+router.get('/home', function (req, res, next) {
+  res.render('index', { page: 'Home' });
+});
+
+
+router.get('/projects', function (req, res, next) {
+  res.render('index', { page: 'Projects' });
+});
+
+router.get('/about-me', function (req, res, next) {
+  res.render('index', { page: 'About Me' });
+});
+
+router.get('/services', function (req, res, next) {
+  res.render('index', { page: 'Services' });
+});
+
+router.get('/contact-me', function (req, res, next) {
+  res.render('index', { page: 'Contact Me' });
+});
+
 
 module.exports = router;
