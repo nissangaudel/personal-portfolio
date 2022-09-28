bbefb459908e4d934cb29ae9e31f94811334ee6b
Nishan Gaudel <nissangaudel@gmial.com>
2022-09-27 22:18:08 -0400

    contact me page added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,34 @@
-window.addEventListener('load', console.log('app is running'));
+const sendButton = document.querySelector('.button');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+sendButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('send button clicked');
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        message: message.value
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/contact-me', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.onreadystatechange = () => {
+        alert('Something Went Wrong');
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+
+            alert('Email Sent Sucessfully');
+            console.log(xhr.responseText)
+
+            message = '';
+        } else {
+            alert('Something Went Wrong');
+        }
+    }
+    xhr.send(JSON.stringify(formData));
+
+});
+
--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,27 @@ router.get('/home', function (req, res, next) {
 
 
 router.get('/projects', function (req, res, next) {
-  res.render('index', { page: 'Projects' });
+  res.render('projects.ejs', { page: 'Projects' });
 });
 
 router.get('/about-me', function (req, res, next) {
-  res.render('index', { page: 'About Me' });
+  res.render('about_me', { page: 'About Me' });
 });
 
 router.get('/services', function (req, res, next) {
-  res.render('index', { page: 'Services' });
+  res.render('services.ejs', { page: 'Services' });
 });
 
 router.get('/contact-me', function (req, res, next) {
-  res.render('index', { page: 'Contact Me' });
+  res.render('contact_me.ejs', { page: 'Contact Me' });
 });
+
+router.post('/contact-me', function (req, res) {
+  console.log(req.body);
+  res.status(200);
+});
+
+
 
 
 module.exports = router;
